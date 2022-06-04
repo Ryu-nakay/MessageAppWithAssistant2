ca50b17432327d29a23eaa5436d4197a9e0fe46b
Ryu-nakay-from_MBA <70783066+Ryu-nakay@users.noreply.github.com>
2022-06-04 15:48:41 +0900

    ログイン画面のUI作成

---
+++ MessageAppWithAssistant/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  MessageAppWithAssistant
+//
+//  Created by 中山龍 on 2022/06/03.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+   /*
+    // ①storyboardのインスタンス取得
+    let storyboard: UIStoryboard = self.storyboard!
+
+    // ②遷移先ViewControllerのインスタンス取得
+    let nextView = storyboard.instantiateViewController(withIdentifier: "SignupView") as! SignupViewController
+
+    // ③画面遷移
+    self.present(nextView, animated: false, completion: nil)
+    */
+
+
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
